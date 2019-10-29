2e4d98764860439abf4bc8c8463514fd227bdbba
Ugonz86 <ugonzalez86@gmail.com>
2019-10-29 08:12:25 -0700

    Adding functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
-
-
-
+console.log("hello");
 });
+
+$("form#sentenceForm").submit(function(event) {
+  var sentenceVariable = $("input#sentenceInput").val();
+  var words = sentenceVariable.split('');
+  var three = [];
+
+  words.forEach(function(word) {
+    if(word.length >= 3) {
+      three.push(word);
+    }
+  });
+  // three.reverse();
+
+  var result = three.join('');
+
+  $("#results").append(result);
+  $("#results").show();
+
+  event.preventDefault();
+  });
