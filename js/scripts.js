121721dbad05d9b14f324df5fb6a760cf58b0600
Ugonz86 <ugonzalez86@gmail.com>
2019-10-29 11:18:00 -0700

    Add fixes to backend functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-console.log("hello");
-});
 
-$("form#sentenceForm").submit(function(event) {
-  var sentenceVariable = $("input#sentenceInput").val();
-  var words = sentenceVariable.split('');
+$("#sentenceForm").submit(function(event) {
+  event.preventDefault();
+  var sentenceVariable = $("#sentenceInput").val();
   var three = [];
-
+  var words = sentenceVariable.split(" ");
   words.forEach(function(word) {
-    if(word.length >= 3) {
+    if(word.split("").length >= 3) {
       three.push(word);
     }
   });
-  // three.reverse();
 
-  var result = three.join('');
+  three.reverse();
 
+  var result = three.join(' ');
+
+  
   $("#results").append(result);
   $("#results").show();
 
-  event.preventDefault();
   });
+});
